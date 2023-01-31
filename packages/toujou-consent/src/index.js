153e8b306a2fb82dae57a7c27505c6bcf611f2ba
Tobias Wojtylak <twojtylak@dfau.de>
2023-01-31 16:59:18 +0100

    feat: add toujou-third-party-content component

--- packages/toujou-consent/src/index.js
+++ packages/toujou-consent/src/index.js
@@ -1,3 +1,5 @@
 import './toujou-consent/toujou-consent';
 import './toujou-consent-widget/toujou-consent-widget';
 import './toujou-tracking';
+import './toujou-third-party-content/toujou-third-party-content';
+
