860664abed08c505b723360443f3af42e0b02778
Tobias Wojtylak <twojtylak@dfau.de>
2023-02-09 10:15:06 +0100

    wip: add inputmask

--- packages/toujou-input-date-mask/src/index.js
+++ packages/toujou-input-date-mask/src/index.js
@@ -1,12 +1,54 @@
 import { LitElement, html} from 'lit';
+import Inputmask from 'inputmask/dist/inputmask.es6.js';
 
 export class ToujouInputDateMask extends LitElement {
     static get is() {
         return 'toujou-input-date-mask';
     }
 
+    static get properties() {
+        return {
+            mask: {
+                type: String,
+            },
+            showMaskOnHover: {
+                type: Boolean,
+            },
+            showMaskOnFocus: {
+                type: Boolean
+            },
+        };
+    }
+
+    constructor() {
+        super();
+        this.mask = 'dd.mm.yyyy';
+        this.showMaskOnHover = false;
+        this.showMaskOnFocus = false;
+    }
+
+    createRenderRoot() {
+        return this;
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        this.inputmask = new Inputmask({
+            mask: this.mask,
+            showMaskOnHover: this.showMaskOnHover,
+            showMaskOnFocus: this.showMaskOnFocus,
+        });
+    }
+
     render() {
-        return html`Hello World`;
+        return html`
+            <input ref={myRef} placeholder={statePlaceholder} type='tel'
+                   onClick={onClick} className={className} spellCheck="false" onInput={onInput} onTouchStart={onTouchStart}
+                   onFocus={onFocus} value={maskOnFocus ? newState : ''} onKeyDown={onKeyDown}
+            autoComplete='off' onPaste={onHandlePaste} onMouseEnter={onHandleMouseEnter}
+            onMouseLeave={onHandleMouseLeave} onBlur={onHandleBlur} disabled={disabled} readOnly={readOnly}></input>
+            
+        `;
     }
 }
 
