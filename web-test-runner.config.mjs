5dea5d91c164974ebf59c67438eb2636601db2bb
Tobias Wojtylak <twojtylak@dfau.de>
2023-02-09 11:34:56 +0100

    tests: adjust web-test-runner.config.mjs for location-finder-test

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -4,14 +4,13 @@ import postcssLitRollup from 'rollup-plugin-postcss-lit';
 import aliasRollup from '@rollup/plugin-alias';
 import commonjsRollup from '@rollup/plugin-commonjs';
 import postcssRollup from 'rollup-plugin-postcss';
-import path from 'path';
+import * as path from 'path';
 
 const replace = fromRollup(rollupReplace);
 const postcss = fromRollup(postcssRollup);
 const postcssLit = fromRollup(postcssLitRollup);
 const alias = fromRollup(aliasRollup);
 const commonjs = fromRollup(commonjsRollup);
-
 const nodeModulesPath = path.resolve('./node_modules');
 
 export default {
@@ -28,12 +27,17 @@ export default {
     plugins: [
         alias({
             resolve: ['', './index.js', '.js'],
+            entries: {
+                '@mapbox/mapbox-gl-geocoder': `${nodeModulesPath}/@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.min.js`,
+            },
         }),
         replace({
             preventAssignment: true,
             'process.env.NODE_ENV': JSON.stringify('production'),
         }),
-        commonjs({}),
+        commonjs({
+            sourceMap: false
+        }),
         postcss({
             inject: false
         }),
