e961ead393354f31a5a7c9efb8c305bdc5b0c017
LuisPato <lpato@dfau.de>
2023-07-14 09:28:26 +0200

    feat(toujou-snackbar): Add the toujou-snackbar package with the component we have been using on kojo's storybook Card: https://app.clickup.com/t/863h779d0

---
+++ packages/toujou-snackbar/tests/index.test.ts
@@ -0,0 +1,17 @@
+import { expect, fixture, html } from '@open-wc/testing';
+
+import '../src/index';
+
+describe('Toujou Snackbar', () => {
+    let element: Element;
+    beforeEach(async () => {
+        element = await fixture(html`
+            <toujou-snackbar></toujou-snackbar>`);
+    });
+
+    it('can create component', async () => {
+        expect(element).to.not.be.null;
+        expect(element).to.not.be.undefined;
+        expect(element.nodeName).to.equal(`toujou-snackbar`.toUpperCase());
+    });
+});
