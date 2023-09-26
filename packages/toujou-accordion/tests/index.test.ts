7e43e935b19fdaf3c307034c15c34c803deeef66
LuisPato <lpato@dfau.de>
2023-09-26 11:30:14 +0200

    fix(ui-components): add simple test file to the toujou-accordion package

---
+++ packages/toujou-accordion/tests/index.test.ts
@@ -0,0 +1,18 @@
+import { expect, fixture, html } from '@open-wc/testing';
+
+import '../src/index';
+
+describe('toujou accordion', () => {
+  let element: Element;
+  beforeEach(async () => {
+    element = await fixture(html`
+      <toujou-accordion></toujou-accordion>
+    `);
+  });
+
+  it('can create component', async () => {
+    expect(element).to.not.be.null;
+    expect(element).to.not.be.undefined;
+    expect(element.nodeName).to.equal('{{package_name}}'.toUpperCase());
+  });
+});
